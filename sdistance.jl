1acd0d8b1ac755ee454d5d97feb44cc14cd564d2
Jabelic <hibiki990523@gmail.com>
2021-09-06 00:35:49 +0900

    add: parallel processing and shell script for test, makefile.

--- sdistance.jl
+++ sdistance.jl
@@ -2,7 +2,6 @@
 # using Plots:length
 # using DataFrames:Matrix
 
-# using Pkg
 # Pkg.add("CSV")
 # using CSV, DataFrames, Plots, DelimitedFiles, Luxor # , GeometricalPredicates
 using CSV, DataFrames, Plots, DelimitedFiles, Luxor, BenchmarkTools
@@ -46,8 +45,8 @@ end
 function judge_para(_x, _y, _gem)
     x_length = length(_x[:,1])
     return_value = zeros(Float64, x_length, x_length)
-    for indexI = 1:length(_y)
-        Threads.@threads for indexJ = 1:length(_x)
+    Threads.@threads for indexI = 1:length(_y)
+        for indexJ = 1:length(_x)
             sdist = 1.0 * distance(_x[indexJ], _y[indexI], _gem)
             if isinside(Point(_x[indexJ], _y[indexI]), [Point(_gem[i,1], _gem[i,2]) for i = 1:length(_gem[:,1])])
                 sdist = (-1) * sdist
@@ -83,12 +82,11 @@ function complement(array, times)
     return tmp
 end
 
-function main()
+function main(N=1000, para_or_serialize_process=1)
     # create the computational domain
-    N = 300
     L = 1.5
     _phi = zeros(Float64, N + 1, N + 1)
-    
+    println("Thread数: ", Threads.nthreads())
     # ganma曲線 data 読み込みちょっと遅いかも. (50 x 2)
     # https://qiita.com/HiroyukiTachikawa/items/e01917ade931031ec6a1
     _gem = readdlm("./interface.csv", ',', Float64)
@@ -99,17 +97,19 @@ function main()
     _y = [i for i = -L:2 * L / N:L] # len:N+1
     
     runtime_ave = 0
-    exetimes = 3
+    exetimes = 4
 
     for i = 1:exetimes
-        _phi, runtime = @timed judge_(_x, _y, _gem)
-        # _phi, runtime = @timed judge_para(_x, _y, _gem)
+        if para_or_serialize_process == 1
+            _phi, runtime = @timed judge_para(_x, _y, _gem) # parallel processing
+        else
+            _phi, runtime = @timed judge_(_x, _y, _gem) # serialize processing
+        end
         runtime_ave += runtime
     end
     println(runtime_ave / exetimes)
 
-    draw(_x, _y, _phi)
+    # draw(_x, _y, _phi)
 end
 
-
-main()
+main(parse(Int, ARGS[1]), parse(Int, ARGS[2]))
---
+++ test.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+# mac(catalina~)
+    # terminalからの実行でマルチスレッド処理を行うには
+    # $ vi ~/.zshrc
+    # export JULIA_NUM_THREADS=`sysctl -n hw.logicalcpu` を書き加える
+    # $ source ~/.zshrc
+    # terminalを新たに起動して
+    # $ julia hogehuga.jl
+#
+
+assert(){
+    input1="$1"
+    input2="$2"
+    input3="$3"
+    # julia sdistance.jl $input1 $input2 --thread `sysctl -n hw.logicalcpu`
+    julia sdistance.jl $input1 $input2
+    actual="$?"
+
+    if [ "$input2" = 1 ]; then
+        echo "並列処理：分割数N=$input1 \n"
+    else
+        echo "直列処理：分割数N=$input1 \n"
+    fi
+}
+
+# assert 100 1
+# assert 100 2
+assert 500 2
+assert 500 1
+# assert 1000 1
+# assert 1000 2
