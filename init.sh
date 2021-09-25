b0db1c47955982c8428dfc09c3dc3b7f8e639570
jabelic <hibiki990523@gmail.com>
2021-09-25 15:36:07 +0900

    fix: init.sh threads num

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 init(){
-    fgrep 'processor' /proc/cpuinfo | wc -l
-    echo "export JULIA_NUM_THREADS=$?" >> ~/.bashrc 
+    THREADS=`fgrep 'processor' /proc/cpuinfo | wc -l`
+    echo "export JULIA_NUM_THREADS=$THREADS" >> ~/.bashrc 
 }
 
 init
