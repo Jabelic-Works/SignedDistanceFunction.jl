6f9f404628f2e5396eff0e895eed4ef5a6d0a961
Jabelic <hibiki990523@gmail.com>
2021-09-30 09:49:28 +0900

    add: performance draw funciton

---
+++ draw.jl
@@ -0,0 +1,17 @@
+module Draw
+    using Plot
+
+    function draw(_x::Array, _y::Array, _phi::Array)
+        s = plot(_x, _y, _phi, st=:wireframe)
+        p = contour(_x, _y, _phi)
+        q = surface(_x, _y, _phi)
+        r = plot(_x, _y, _phi, st=:heatmap)
+        plot(s, p, q, r, layout=(4, 1), size=(500, 1200))
+        savefig("signed_distance.png")
+    end
+
+    function parformance_graphs(exe_num::Array, N::Array)
+        plot(N, exe_num)
+        savefig("performance.png")
+    end
+end
--- main.jl
+++ main.jl
@@ -1,11 +1,16 @@
 using DataFrames, CSV
 include("./sdistance.jl") # 必ずダブルクオーテーション
 include("./generate_data.jl") 
+include("./draw.jl") 
 import .Sdistance:main
 import .Generate:get_mock_data
+import .Draw:parformance_graphs
 
 # tmp = get_mock_data(1.5, 100)
 # df = DataFrame(x=tmp[:,1], y=tmp[:,2])
 # CSV.write("circle.csv", df, writeheader=false)
 main(parse(Int, ARGS[1]), parse(Int, ARGS[2]), "./interface.csv")
 # main(parse(Int, ARGS[1]), parse(Int, ARGS[2]), "./circle.csv")
+
+parformance_graphs()
+
