c25aaae15e4fcffe2e8f811c922e2582d0fbde3c
Jabelic <hibiki990523@gmail.com>
2021-10-28 09:07:46 +0900

    add test case

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,7 @@ using DelimitedFiles
 
 @testset "LevelSet.jl" begin
     @test signedDistance2D("mock_csv_data/interface.csv", 100) == readdlm("result/interface_result.csv", ',', Float64)
+    @test signedDistance2D("mock_csv_data/interface.csv", 500) == readdlm("result/interface_result_n500.csv", ',', Float64)
     @test signedDistance2D("mock_csv_data/multiple_curves.csv", 100, "multi") == readdlm("result/multiple_curves_result.csv", ',', Float64)
+    @test signedDistance2D("mock_csv_data/multiple_curves.csv", 500, "multi") == readdlm("result/multiple_curves_result_n500.csv", ',', Float64)
 end
